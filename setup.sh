7e29530ca0744434694b1e268ed57883a988199a
Geoff Greer <geoff@greer.fm>
2017-05-08 17:41:06 -0700

    Add example pf.conf

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo route -nv add -net 172.18.0.0/24 -interface vboxnet0
 sudo sysctl -w net.inet.ip.forwarding=1
 
 echo 'net.inet.ip.forwarding=1' | sudo tee -a /etc/sysctl.conf > /dev/null
-eval echo "load anchor \"com.apple.vboxnet0\" from \"$(pwd)/etc/pf.conf\"" | sudo tee -a /etc/pf.conf > /dev/null
+eval echo "load anchor \"example-tectonic.vboxnet0\" from \"$(pwd)/etc/pf.conf\"" | sudo tee -a /etc/pf.conf > /dev/null
 
 sudo pfctl -e -f /etc/pf.conf 
 sudo defaults write /System/Library/LaunchDaemons/com.apple.pfctl ProgramArguments '(pfctl, -f, /etc/pf.conf, -e)'
